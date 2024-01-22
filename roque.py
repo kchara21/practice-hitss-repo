707b4ab149f619664aaf402cebc7343223b514f0
Roque Armando Ramirez Pionce <109245164+Armando8bits@users.noreply.github.com~>
2024-01-22 10:54:19 -0500

    first commit

--- roque.py
+++ roque.py
@@ -1,3 +1,13 @@
 #este es un comentario
 
 #Este es un cambio para el pull request
+
+url="https://previexappdev.azurewebsites.net/Contrato/EditEmpresarial/395"
+ID_Contrato = ""
+i = len(url) - 1
+
+while i >= 0 and url[i].isdigit():
+    ID_Contrato = url[i] + ID_Contrato
+    i -= 1
+
+print (ID_Contrato)
